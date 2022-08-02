75a9b3a4bd60a5bd0ee7bad231cdd134b91c00a8
Alaa Aouni <aaouni@e1r6p11.1337.ma>
2022-08-02 15:56:13 +0100

    finally

---
+++ pipex_utils.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaouni <aaouni@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 15:40:16 by aaouni            #+#    #+#             */
+/*   Updated: 2022/08/02 15:26:01 by aaouni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	len_2d(char **str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		;
+	return (i);
+}
+
+void	free_2d(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{	
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+char	**get_path_env(char **env)
+{
+	char	**path;
+	char	**path_slash;
+	char	*path_str;
+	t_cmpt	t;
+
+	t.j = -1;
+	t.i = 0;
+	while (env[t.i])
+	{
+		if (ft_strncmp(env[t.i], "PATH", 4) == 0)
+		{
+			path_str = env[t.i] + 5;
+			path = ft_split(path_str, ':');
+			path_slash = malloc (sizeof(char *) * (len_2d(path) + 1));
+			if (!path_slash)
+				return (NULL);
+			while (path[++t.j])
+				path_slash[t.j] = ft_strjoin(path[t.j], "/");
+			free_2d(path);
+			path_slash[t.j] = NULL;
+			return (path_slash);
+		}
+		t.i++;
+	}
+	return (NULL);
+}
+
+char	**get_path_cmd(char *str, char **path)
+{
+	int		i;
+	char	**cmd;
+	char	*path_cmd;
+
+	i = 0;
+	cmd = ft_split(str, ' ');
+	if (access(cmd[0], X_OK) == 0)
+		return (cmd);
+	while (path[i])
+	{
+		path_cmd = ft_strjoin(path[i], cmd[0]);
+		if (access(path_cmd, X_OK) == 0)
+		{
+			free (cmd [0]);
+			cmd[0] = ft_strdup(path_cmd);
+			free(path_cmd);
+			return (cmd);
+		}
+		free(path_cmd);
+		i++;
+	}
+	return (NULL);
+}
